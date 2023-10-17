2c2ad89d73b09e6b55fb643fe518a370f4efe8f3
Avin-experion <avin.joseph@experionglobal.com>
2023-10-17 15:22:47 +0530

    Added files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Table from './components/Table'
+
+const App = () => {
+  return (
+    <div className='main'>
+      <Table />
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+
